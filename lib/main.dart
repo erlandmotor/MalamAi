83bd6c821587451142e10aef2e0ecb3e59d8e911
metalgeni <metalgeni@paran.com>
2023-05-23 21:25:14 +0900

    first notebook

--- lib/main.dart
+++ lib/main.dart
@@ -118,10 +118,10 @@ class ChatApp extends StatefulWidget {
   final ChatApi chatApi;
 
   @override
-  State<ChatApp> createState() => _ChatAppState();
+  State<ChatApp> createState() => ChatAppState();
 }
 
-class _ChatAppState extends State<ChatApp> {
+class ChatAppState extends State<ChatApp> {
   @override
   Widget build(BuildContext context) {
     var uiNoti = context.watch<UIChangeNotifier>();
